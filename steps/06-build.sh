d343dcbc20b8e80d73a79d4816ebc1a411e0dc06
David Sungaila <david.sungaila@arcor.de>
2025-08-31 00:04:35 +0200

    Build pdfium with WASM exceptions (instead of emscripten exceptions)

--- steps/06-build.sh
+++ steps/06-build.sh
@@ -17,6 +17,9 @@ if [ "$TARGET_CPU" == "wasm" ]; then
     -s EXPORTED_RUNTIME_METHODS="ccall,cwrap,addFunction,removeFunction"
     -s LLD_REPORT_UNDEFINED
     -s WASM=1
+    -fwasm-exceptions
+    -sDISABLE_EXCEPTION_CATCHING=0
+    -sSUPPORT_LONGJMP=wasm
     -o "$BUILD_DIR/pdfium.html"
     "$LIBPDFIUMA"
     --no-entry
