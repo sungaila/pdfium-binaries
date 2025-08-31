24f5d9fb85b81df3c52c886465b2d2d91c734f87
David Sungaila <david.sungaila@arcor.de>
2025-08-31 11:40:28 +0200

    update

--- steps/02-checkout.sh
+++ steps/02-checkout.sh
@@ -7,7 +7,12 @@ ENABLE_V8=${PDFium_ENABLE_V8:-false}
 CONFIG_ARGS=()
 # Clone
 CONFIG_ARGS+=( --custom-var "checkout_configuration=default" )
-gclient config --unmanaged "$PDFium_URL" "${CONFIG_ARGS[@]-}" --target-os=linux,${OS}
+gclient config --unmanaged "$PDFium_URL" "${CONFIG_ARGS[@]-}"
+if grep -q '^[[:space:]]*target_os' .gclient; then
+  sed -i -E "s|^[[:space:]]*target_os\s*=.*|target_os = [ \"linux\", \"${OS}\" ]|g" .gclient
+else
+  printf "\n%s\n" "target_os = [ \"linux\", \"${OS}\" ]" >> .gclient
+fi
 
 # Reset
 for FOLDER in pdfium pdfium/build pdfium/v8 pdfium/third_party/libjpeg_turbo pdfium/base/allocator/partition_allocator; do
