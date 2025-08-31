350bcb596e8378a2a6b68f8dffadeb8baabc111e
David Sungaila <david.sungaila@arcor.de>
2025-08-31 10:52:13 +0200

    Update scripts

--- steps/05-configure.sh
+++ steps/05-configure.sh
@@ -49,7 +49,7 @@ mkdir -p "$BUILD"
       echo 'pdf_is_complete_lib = true'
       echo 'is_clang = false'
       echo 'use_custom_libcxx = false'
-      if true; then
+      if [ "$ENABLE_V8" == "true" ]; then
         # Set a toolchain with the same bitness as the target CPU
         echo "v8_snapshot_toolchain = \"//build/toolchain/linux:x86\""
         # Don't try to build libc++ because it requires GCC 14+
