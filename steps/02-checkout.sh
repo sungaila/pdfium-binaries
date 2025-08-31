299f7a0f05fd6676a60b2caa4207dbded66c45ca
David Sungaila <david.sungaila@arcor.de>
2025-08-31 11:35:03 +0200

    Update scripts

--- steps/02-checkout.sh
+++ steps/02-checkout.sh
@@ -7,11 +7,7 @@ ENABLE_V8=${PDFium_ENABLE_V8:-false}
 CONFIG_ARGS=()
 # Clone
 CONFIG_ARGS+=( --custom-var "checkout_configuration=default" )
-gclient config --unmanaged "$PDFium_URL" "${CONFIG_ARGS[@]-}"
-cat >> .gclient <<EOF
-target_os = [ "linux", "$OS" ]
-EOF
-
+gclient config --unmanaged "$PDFium_URL" "${CONFIG_ARGS[@]-}" --target-os=linux,${OS}
 
 # Reset
 for FOLDER in pdfium pdfium/build pdfium/v8 pdfium/third_party/libjpeg_turbo pdfium/base/allocator/partition_allocator; do
@@ -24,6 +20,6 @@ done
 gclient sync -r "origin/${PDFium_BRANCH:-main}" --no-history --shallow --reset --force --delete_unversioned_trees
 gclient runhooks
 
-test -f pdfium/third_party/libjpeg_turbo/jerror.h && echo ok || echo missing
-test -f pdfium/third_party/libjpeg_turbo/jpeglib.h && echo ok || echo missing
-test -f pdfium/gen/third_party/libjpeg_turbo/jconfig.h && echo gen-ok || echo gen-missing
+test -f pdfium/third_party/libjpeg_turbo/jerror.h   && echo "jerror.h ok"   || echo "jerror.h missing"
+test -f pdfium/third_party/libjpeg_turbo/jpeglib.h  && echo "jpeglib.h ok"  || echo "jpeglib.h missing"
+test -f pdfium/gen/third_party/libjpeg_turbo/jconfig.h && echo "gen jconfig.h ok" || echo "gen jconfig.h missing"
